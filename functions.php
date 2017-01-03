3e3bfb8ea35a98d0dcd947043a0f2003dcb73b04
Lukas Hahmann <tfr.didi@gmail.com>
2017-01-03 15:28:32 +0100

    Man kann jetzt einen Facebook-Link und einen Github-Link übers Admin-Interface pflegen

--- functions.php
+++ functions.php
@@ -10,4 +10,45 @@ add_action( 'wp_enqueue_scripts', 'startwordpress_scripts' );
 
 // WordPress Titles
 add_theme_support( 'title-tag' );
+
+// Custom settings
+function custom_settings_add_menu() {
+  add_menu_page( 'Theme Settings', 'Theme Settings', 'manage_options', 'custom-settings', 'custom_settings_page', null, 99 );
+}
+add_action( 'admin_menu', 'custom_settings_add_menu' );
+
+// Create Custom Global Settings
+function custom_settings_page() { ?>
+  <div class="wrap">
+    <h1>Theme Settings</h1>
+    <form method="post" action="options.php">
+       <?php
+           settings_fields( 'section' );
+           do_settings_sections( 'theme-options' );      
+           submit_button(); 
+       ?>          
+    </form>
+  </div>
+<?php }
+
+// Facebook
+function setting_facebook() { ?>
+  <input type="text" name="facebook" id="facebook" value="<?php echo get_option( 'facebook' ); ?>" />
+<?php }
+
+// Github
+function setting_github() { ?>
+  <input type="text" name="github" id="github" value="<?php echo get_option('github'); ?>" />
+<?php }
+
+function custom_settings_page_setup() {
+  add_settings_section( 'section', 'All Settings', null, 'theme-options' );
+  add_settings_field( 'Facebook', 'Facebook URL', 'setting_facebook', 'theme-options', 'section' );
+  add_settings_field( 'github', 'GitHub URL', 'setting_github', 'theme-options', 'section' );
+  
+  register_setting( 'section', 'facebook' );
+  register_setting( 'section', 'github' );
+}
+add_action( 'admin_init', 'custom_settings_page_setup' );
+
 ?>
