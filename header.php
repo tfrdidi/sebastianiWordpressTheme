0ec9c41058cceaa0a8b79e825a6a77b89ae18094
Lukas Hahmann <tfr.didi@gmail.com>
2017-01-03 11:51:48 +0100

    Titel und Beschreibung sind generisch

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Blog Template for Bootstrap</title>
+    <title><?php echo get_bloginfo( 'name' ); ?></title>
 
     <!-- Bootstrap core CSS -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
@@ -37,6 +37,6 @@
     <div class="container">
 
       <div class="blog-header">
-        <h1 class="blog-title">The Bootstrap Blog</h1>
-        <p class="lead blog-description">The official example template of creating a blog with Bootstrap.</p>
+        <h1 class="blog-title"><a href="<?php bloginfo( 'wpurl' );?>"><?php echo get_bloginfo( 'name' ); ?></a></h1>
+        <p class="lead blog-description"><?php echo get_bloginfo( 'description' ); ?></p>
       </div>
