9957d193784ecb2d9c9923e36abf1bb3e044e5c2
Lukas Hahmann <tfr.didi@gmail.com>
2017-01-03 11:57:34 +0100

    Jetzt können Inhalte übers CMS gepflegt werden

--- content.php
+++ content.php
@@ -1,10 +1,7 @@
 <div class="blog-post">
-	<h2 class="blog-post-title">Sample blog post</h2>
-	<p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
+	<h2 class="blog-post-title"><?php the_title(); ?></h2>
+	<p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
 
-	<p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
-	<hr>
-
-<!-- the rest of the content -->
+ <?php the_content(); ?>
 
 </div><!-- /.blog-post -->
--- index.php
+++ index.php
@@ -4,7 +4,13 @@
 
 		<div class="col-sm-8 blog-main">
 
-			<?php get_template_part( 'content', get_post_format() ); ?>
+			<?php 
+			if ( have_posts() ) : while ( have_posts() ) : the_post();
+  	
+				get_template_part( 'content', get_post_format() );
+  
+			endwhile; endif; 
+			?>
 
 		</div> <!-- /.blog-main -->
 
